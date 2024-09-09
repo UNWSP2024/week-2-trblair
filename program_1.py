40514dd701f461a7ab96898f08e81927fd3b4dda
trblair <trblair@students.unwsp.edu>
2024-09-09 16:29:56 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,12 +1,9 @@
 #Tristan Blair's Personal information
 #Program was written on 9/9/24
-
 def personal_information():
     print("Tristan Blair")
     print("792 County Rd C W, Roseville MN, 55113")
     print("(763)-548-4388")
     print("Undeclared")
-
-
 # Line which calls the above function.
 personal_information()
